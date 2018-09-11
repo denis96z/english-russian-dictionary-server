25b8b1a1bfbddcc975f019c813cec32a6a43c2e9
denis96z <zinoviev.denis@yandex.ru>
2018-09-11 13:55:03 +0300

    Refactoring

--- src/main/kotlin/ru/bmstu/iu7/englishrussiandictionaryserver/domain/Word.kt
+++ src/main/kotlin/ru/bmstu/iu7/englishrussiandictionaryserver/domain/Word.kt
@@ -1,6 +1,11 @@
 package ru.bmstu.iu7.englishrussiandictionaryserver.domain
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
 data class Word(
+        @Id
         var id: String = "",
         var inEnglish: String = "",
         var inRussian: String = ""
