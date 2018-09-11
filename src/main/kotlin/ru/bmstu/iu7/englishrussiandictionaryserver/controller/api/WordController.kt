864f0627052cc451b5cd7e8e6640a8729127bdc2
denis96z <zinoviev.denis@yandex.ru>
2018-09-11 13:48:50 +0300

    WordController added

---
+++ src/main/kotlin/ru/bmstu/iu7/englishrussiandictionaryserver/controller/api/WordController.kt
@@ -0,0 +1,28 @@
+package ru.bmstu.iu7.englishrussiandictionaryserver.controller.api
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import ru.bmstu.iu7.englishrussiandictionaryserver.exception.NotFoundException
+import ru.bmstu.iu7.englishrussiandictionaryserver.service.WordService
+
+@RestController
+@RequestMapping("/api/words")
+class WordController(@Autowired
+                     private val wordService: WordService) {
+    @GetMapping("/english/{word}")
+    fun translateWordFromEnglishIntoRussian(@PathVariable word: String) : String {
+        return wordService.translateWordFromEnglishIntoRussian(word)
+    }
+
+    @GetMapping("/russian/{word}")
+    fun translateWordFromRussianIntoEnglish(@PathVariable word: String) : String {
+        return wordService.translateWordFromRussianIntoEnglish(word)
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(exception: NotFoundException) : String? {
+        return exception.message
+    }
+}
